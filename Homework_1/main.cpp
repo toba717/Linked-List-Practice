1f03000cf90136c9857cdce1d343848c7dd803bb
toba717 <toba7171@gmail.com>
2022-07-08 18:01:32 -1000

    Initial Commit

---
+++ Homework_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Homework_1
+//
+//  Created by Takao Oba on 7/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
